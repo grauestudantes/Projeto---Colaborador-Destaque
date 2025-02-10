ffcfe6c43a59a09fee273d0e7480623ea561d7f6
Grau Estudantes <grauestudantes@gmail.com>
2025-02-10 08:47:15 -0300

    Reconstrução de validações dos campos

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
-const btnLogin = document.querySelector('button#btnLogin');
+const btnLogin = document.querySelector('#btnLogin');
 
 btnLogin.addEventListener('click', function() {
 
-    const nome = document.querySelector('input#txtNome').value;
-    const senha = document.querySelector('input#passWord').value;
+    const nome = document.querySelector('#txtNome').value;
+    const senha = document.querySelector('#passWord').value;
 
-    const nomeVazio = (nome.trim === "");
-    const senhaVazia = (nome.trim === "");
+    const nomeVazio = (nome.trim() === '');
+    const senhaVazia = (senha.trim() === '');
+
+    const nomePadrao = (/^[a-zA-ZÀ-ÿ\s']{3,}$/)
+    const senhaPadrao = /^[a-zA-Z0-9#$]{3,}$/
+    const nomeValido = nomePadrao.test(nome)
+    const senhaValida = senhaPadrao.test(senha)
+
 
     if (nomeVazio || senhaVazia) {
         alert('Preencha as informações corretamente!')
+        return;
+    }
+
+    if (!(nomeValido)) {
+        alert('Nome invalido!')
+        return;
+    } else if (!(senhaValida)) {
+        alert('Senha invalida!')
+        return;
     }
 });
